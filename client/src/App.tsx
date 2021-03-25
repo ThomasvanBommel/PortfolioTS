4ef84b6f91f927806c5e20cae07322818f17d605
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-03-25 01:32:22 -0300

    Cleaned up App by addeding a Main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@
  * Created: Saturday February 6th 2021
  * Author: Thomas vanBommel
  * 
- * Last Modified: Thursday March 25th 2021 12:43am
+ * Last Modified: Thursday March 25th 2021 1:31am
  * Modified By: Thomas vanBommel
  * 
  * CHANGELOG:
@@ -13,56 +13,34 @@
  * 2021-03-13	TvB	Updated header + Change import location
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from './navbar/Navbar';
-import YoutubeCarousel from './youtube/YoutubeCarousel';
-import ContactForm from './contact/ContactForm';
 import Carousel from "./youtube/Carousel2";
+import ContactForm from './contact/ContactForm';
 import ProgressBanner from "./progress_banner/ProgressBanner";
 
-// import store from 
-import { Provider } from "react-redux";
-import { Page } from "./pageReducer";
-// import { createStore } from "redux";
+import { Provider, useSelector } from "react-redux";
+import { Page, getPage } from "./pageReducer";
 import store from "./store";
 
-// export enum Page {
-//     Home = "Home", 
-//     Contact = "Contact"
-// };
-
-// const store = createStore(
-//     pageReducer,
-//     // @ts-ignore
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-
-
 function App(){
-    // const [ activePage, setActivePage ] = useState(Page.Home);
-
-    // Temporary until navbar is refactored
-    // const handleContactClick = () => {
-    //     setActivePage(activePage === Page.Home ? Page.Contact : Page.Home);
-    // };
-
-
     return (
         <Provider store={ store }>
             <Navbar />
+            <Main />
             <ProgressBanner />
-            {
-                store.getState().page.page === Page.Home ? (
-                    <div>
-                        <Carousel />
-                    </div>
-                ) : (
-                    <ContactForm />
-                )
-            }
         </Provider>
     );
+}
+
+function Main(){
+    let page = useSelector(getPage);
+
+    if(page === Page.Home){
+        return <Carousel />
+    }
+
+    return <ContactForm />
 }
 
 export default App;

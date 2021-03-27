1b9475828171ce4d8b5d11f6b4ccf330a17ff947
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-03-27 04:08:56 -0300

    client + config cleanup

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+/*
+ * File: webpack.config.js
+ * Created: Sunday February 14th 2021
+ * Author: Thomas vanBommel
+ * 
+ * Last Modified: Saturday March 27th 2021 4:01am
+ * Modified By: Thomas vanBommel
+ * 
+ * CHANGELOG:
+ * 2021-03-27	TvB	Changed over to be always production mode
+ * 2021-03-13   TvB	Updated header
+ */
+
+const join = require("path").join;
+
+// export webpack config
+module.exports = {
+    mode: "production",
+
+    entry: { client: join(__dirname, "src/index.tsx") },
+    output: { path:  join(__dirname, "build"), filename: "bundle.js" },
+
+    resolve: { extensions: [".ts", ".tsx", ".js", ".jsx", ".css"] },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: "ts-loader" },
+            { test: /\.css$/,  use: ["style-loader", "css-loader"] }
+        ]
+    }
+};
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
  # Created: Thursday March 25th 2021
  # Author: Thomas vanBommel
  # 
- # Last Modified: Saturday March 27th 2021 1:46am
+ # Last Modified: Saturday March 27th 2021 3:56am
  # Modified By: Thomas vanBommel
  # 
  # CHANGELOG:
@@ -22,17 +22,15 @@ echo "Building server:"
 tsc --build server/src && echo " - Done" || echo " - Failed"
 
 ## Build client
-# TODO: move config file to client/
 echo "Webpacking client:"
-webpack --config config/webpack.config.js
+webpack --config client/webpack.config.js
 
 ## If testing, clean up and return successful
 if [ "$1" = "test" ]; then
-    scripts/cleanup.sh
+    # scripts/cleanup.sh
     exit 0
 fi
 
 ## Start server
 echo "Starting server:"
-# node server/src/build/index.js
 ROOT=$(pwd) node server/build/src/index.js

344a9efb7a11b727f22f63b98afe432d1ecbf5ca
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-04-20 16:28:49 -0300

    Added login form

--- client/src/admin/AdminLogin.tsx
+++ client/src/admin/AdminLogin.tsx
@@ -3,26 +3,47 @@
  * Created: Tuesday April 20th 2021
  * Author: Thomas vanBommel
  * 
- * Last Modified: Tuesday April 20th 2021 4:10pm
+ * Last Modified: Tuesday April 20th 2021 4:28pm
  * Modified By: Thomas vanBommel
  * 
  * CHANGELOG:
  */
 
-import React from "react";
-
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 
+import Spinner from "../spinner/Spinner";
 import style from "./AdminLogin.module.css";
 
 function AdminLogin(){
+    const [ loading, setLoading ] = useState(false);
+
+    function handleSubmit(event: React.FormEvent){
+        event.preventDefault();
+        event.stopPropagation();
+
+        setLoading(true);
+    }
+
+    if(loading)
+        return (
+            <Spinner />
+        );
+
     return (
-        <div>
+        <div className={ style.container }>
             <Helmet>
                 <title>Admin: vanbommel.ca</title>
                 <meta name="description" content="Admin panel" />
             </Helmet>
-            Admin Login
+            <p>Admin Login</p>
+            <form action="#" method="POST" onSubmit={ handleSubmit }>
+                <input type="text" name="username" placeholder="Username" />
+                <br/>
+                <input type="password" name="password" placeholder="Password" />
+                <br/>
+                <input type="submit" value="Login"/>
+            </form>
         </div>
     );
 }
